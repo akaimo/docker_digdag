bea481cc441048e67bc86c1943026b2da0572f3d
akaimo <kawashu0723@gmail.com>
2019-06-17 17:51:47 +0900

    import other script

--- workflows/tasks/exec-python.py
+++ workflows/tasks/exec-python.py
@@ -1,6 +1,15 @@
 import digdag
 import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+
+import func
 
 
 def version():
     print(sys.version)
+
+
+def date():
+    func.date()
